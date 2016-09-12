f52b72871e895305b4b725da3c342a90577c77c3
Di Zhang <zhangdi_me@163.com>
2016-09-12 11:01:10 +0800

    增加 vagrant up 时执行命令

--- vagrant/provision/once-as-root.sh
+++ vagrant/provision/once-as-root.sh
@@ -18,4 +18,16 @@ sed -i 's/APACHE_RUN_GROUP=www-data/APACHE_RUN_GROUP=vagrant/g' /etc/apache2/env
 
 info "Enabling site configuration"
 ln -s /apps/vagrant/apache/apps.conf /etc/apache2/sites-enabled/apps.conf
+
+info "Install Composer Vendor"
+composer config -g repo.packagist composer https://packagist.phpcomposer.com
+composer global require "fxp/composer-asset-plugin:~1.1.1"
+cd /apps && composer install -vvv --prefer-dist
+
+info "Create Database on VM first run"
+mysql -uroot -proot -e "create database if not exists yiizh default charset utf8 collate utf8_unicode_ci;"
+
+info "Apply database migrations."
+php /apps/bin/console migrate --interactive=0 --migrationPath=@yii/rbac/migrations
+php /apps/bin/console migrate --interactive=0
 echo "Done!"
