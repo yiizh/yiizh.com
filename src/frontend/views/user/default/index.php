7f6caa359e3d63560f731b87d39ff530354fc7e6
Di Zhang <zhangdi_me@163.com>
2016-09-13 15:46:11 +0800

    修改 用户个人主页标题

--- src/frontend/views/user/default/index.php
+++ src/frontend/views/user/default/index.php
@@ -5,6 +5,7 @@
  * @license http://www.yiizh.com/license/
  */
 
+use common\models\User;
 use yii\data\ActiveDataProvider;
 use yii\web\View;
 use yii\widgets\ListView;
@@ -12,8 +13,9 @@ use yii\widgets\ListView;
 /**
  * @var $this View
  * @var $activityProvider ActiveDataProvider
+ * @var $model User
  */
-$this->title = '';
+$this->title = $model->name;
 ?>
 <div class="user-default-index">
     <?= ListView::widget([
