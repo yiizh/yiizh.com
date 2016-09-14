3776443b6225f5fb638dd56cc1be640898a2070b
Di Zhang <zhangdi_me@163.com>
2016-09-14 15:22:57 +0800

    去掉 动态模块缓存机制

--- src/common/components/bootstrap/ModuleAutoLoader.php
+++ src/common/components/bootstrap/ModuleAutoLoader.php
@@ -16,31 +16,22 @@ use yii\log\Logger;
 
 class ModuleAutoLoader extends Object implements BootstrapInterface
 {
-    const CACHE_KEY = 'module-configs';
-
     public function bootstrap($app)
     {
         try {
-            $modules = Yii::$app->cache->get(self::CACHE_KEY);
-            if ($modules === false) {
-                $modules = [];
-
-                $models = Module::find()->all();
-                $modulePath = Yii::getAlias(Yii::$app->params['moduleAutoloadPath']);
-                foreach ($models as $model) {
-                    $moduleDir = $modulePath . DIRECTORY_SEPARATOR . $model->moduleId . '-' . $model->version;
-                    if (is_dir($moduleDir) && is_file($moduleDir . DIRECTORY_SEPARATOR . 'config.php')) {
-                        try {
-                            $modules[$moduleDir] = require($moduleDir . DIRECTORY_SEPARATOR . 'config.php');
-                        } catch (\Exception $ex) {
-                        }
+            $modules = [];
+            $models = Module::find()->all();
+            $modulePath = Yii::getAlias(Yii::$app->params['moduleAutoloadPath']);
+            foreach ($models as $model) {
+                $moduleDir = $modulePath . DIRECTORY_SEPARATOR . $model->moduleId . '-' . $model->version;
+                if (is_dir($moduleDir) && is_file($moduleDir . DIRECTORY_SEPARATOR . 'config.php')) {
+                    try {
+                        $modules[$moduleDir] = require($moduleDir . DIRECTORY_SEPARATOR . 'config.php');
+                    } catch (\Exception $ex) {
                     }
                 }
-
-                if (!YII_DEBUG) {
-                    Yii::$app->cache->set(self::CACHE_KEY, $modules);
-                }
             }
+
             foreach ($modules as $basePath => $config) {
                 Yii::$app->moduleManager->register($basePath, $config);
             }
