f23727b1a742cdb650e12fb0100d5ae00f4c4ce9
Di Zhang <zhangdi_me@163.com>
2016-09-14 11:31:15 +0800

    为了增加 composer 安装速度, 将 composer 缓存到宿主机

--- deploy/run.sh
+++ deploy/run.sh
@@ -4,6 +4,7 @@ set -e
 
 if [ ! -d "$WORKDIR/vendor" ]; then
     cd $WORKDIR && \
+    composer config -g cache-dir /opt/composer/cache && \
     composer config -g repo.packagist composer https://packagist.phpcomposer.com && \
     composer global require "fxp/composer-asset-plugin:~1.1.1" && \
     composer install -vvv --prefer-dist --no-dev --optimize-autoloader
